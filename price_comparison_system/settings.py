a51c4f3ce3ccdf9eb9c1fec3ba19d427c1623f24
DimaPr7 <dprokhach@gmail.com>
2026-05-23 21:12:52 +0300

    fix static files with whitenoise

--- price_comparison_system/settings.py
+++ price_comparison_system/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'price_comparison_system.urls'
@@ -129,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
